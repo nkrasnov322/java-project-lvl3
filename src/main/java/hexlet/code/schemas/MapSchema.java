85918c6b495d27eb81fa551a56a3211d4084c35f
Nikolay Krasnov <nikolay@krasnov.cf>
2022-02-16 13:21:39 +0300

    исправления оформления кода

--- src/main/java/hexlet/code/schemas/MapSchema.java
+++ src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,7 +2,7 @@ package hexlet.code.schemas;
 
 import java.util.Map;
 
-public class MapSchema extends BaseSchema {
+public final class MapSchema extends BaseSchema {
 
     public void required() {
         addPredicate((x) -> (x instanceof Map));
